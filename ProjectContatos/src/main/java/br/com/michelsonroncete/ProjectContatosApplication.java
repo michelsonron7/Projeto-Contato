f23616ade8d4d589c33fb62486bb16522769ec9d
michelsonron7 <michelsonroncete@gmail.com>
2024-03-13 23:21:40 -0300

    Crud - contatos

---
+++ ProjectContatos/src/main/java/br/com/michelsonroncete/ProjectContatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.michelsonroncete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectContatosApplication.class, args);
+	}
+
+}
